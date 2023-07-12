232b6d893d0f634281bdbc984961b8aec76a0ecf
PhillipDuarte1 <phillipito2000@gmail.com>
2023-07-11 20:00:34 -0600

    changed cards dynamic to be unique

--- components/CardPack.js
+++ components/CardPack.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { db, auth } from '../utils/firebase';
 import Card from './Card';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 const CardPack = ({ packName }) => {
   const [cards, setCards] = useState([]);
@@ -9,68 +11,61 @@ const CardPack = ({ packName }) => {
   const openPack = () => {
     auth.onAuthStateChanged((user) => {
       if (user) {
-        db.ref(`users/${user.uid}/collection/${packName}/cards`).once('value', (snapshot) => {
-          const collection = snapshot.val() || {};
-          const collectionArray = Object.values(collection);
-          db.ref(`packs/${packName}/cards`).once('value', (snapshot) => {
-            const cards = snapshot.val();
-            const cardArray = Object.values(cards);
-            const selectedCards = [];
-            let totalRate = 0;
+        db.ref(`packs/${packName}/cards`).once('value', (snapshot) => {
+          const cards = snapshot.val();
+          const cardArray = Object.values(cards);
+          const selectedCards = [];
+          const timestamp = Date.now();
+          let totalRate = 0;
+
+          for (const card of cardArray) {
+            totalRate += card.rate;
+          }
+
+          for (let i = 0; i < 5; i++) {
+            const randomNumber = Math.random() * 100;
+            let cardType = 'common';
+            let cumulativeRate = 0;
 
             for (const card of cardArray) {
-              totalRate += card.rate;
+              const normalizedRate = (card.rate / totalRate) * 100;
+              cumulativeRate += normalizedRate;
+              if (randomNumber <= cumulativeRate) {
+                cardType = card.type;
+                break;
+              }
             }
 
-            const timestamp = Date.now();
+            const filteredCards = cardArray.filter((card) => card.type === cardType);
+            const randomIndex = Math.floor(Math.random() * filteredCards.length);
+            const selectedCard = filteredCards[randomIndex];
 
-            for (let i = 0; i < 5; i++) {
-              const randomNumber = Math.random() * 100;
-              let cardType = 'common';
-              let cumulativeRate = 0;
+            const pulledGlobalCountRef = db.ref(`packs/${packName}/cards/${selectedCard.id}/globalCount`);
 
-              for (const card of cardArray) {
-                const normalizedRate = (card.rate / totalRate) * 100;
-                cumulativeRate += normalizedRate;
-                if (randomNumber <= cumulativeRate) {
-                  cardType = card.type;
-                  break;
+            pulledGlobalCountRef.transaction((currentCount) => (currentCount || 0) + 1, (err, committed, snapshot) => {
+              if (committed) {
+                const pulledGlobalCount = snapshot.val();
+
+                const newCard = {
+                  ...selectedCard,
+                  uid: uuidv4(),
+                  packName: packName,
+                  lastAcquired: timestamp + i,
+                  pulledGlobalCount: pulledGlobalCount,
+                };
+
+                selectedCards.push(newCard);
+
+                if (selectedCards.length === 5) {
+                  for (const card of selectedCards) {
+                    const { globalCount, ...cardData } = card;
+                    db.ref(`users/${user.uid}/collection/cards/${cardData.uid}`).set(cardData);
+                  }
+                  setCards(selectedCards);
                 }
               }
-
-              const filteredCards = cardArray.filter((card) => card.type === cardType);
-              const randomIndex = Math.floor(Math.random() * filteredCards.length);
-              const selectedCard = filteredCards[randomIndex];
-
-              let cardExists = false;
-              let existingCard;
-              for (const card of collectionArray) {
-                if (card.id === selectedCard.id) {
-                  cardExists = true;
-                  existingCard = card;
-                  break;
-                }
-              }
-
-              if (cardExists) {
-                existingCard.count += 1;
-                selectedCard.count = existingCard.count;
-              } else {
-                selectedCard.count = selectedCards.filter(card => card.id === selectedCard.id).length + 1;
-              }
-
-              selectedCard.lastAcquired = timestamp + i;
-              selectedCards.push(selectedCard);
-            }
-
-            for (const card of selectedCards) {
-              db.ref(`users/${user.uid}/collection/${packName}/cards/${card.id}`).set(card);
-            }
-            setCards([]);
-            setCards(selectedCards);
-          }).then(() => {
-
-          });
+            });
+          }
         });
       }
     });
@@ -80,8 +75,8 @@ const CardPack = ({ packName }) => {
     <View style={styles.container}>
       <Button onPress={openPack} title='Open Card Pack' />
       <View style={styles.cardContainer}>
-        {cards.map((card, index) => (
-          <Card key={index} card={card} />
+        {cards.map((card) => (
+          <Card key={card.uid} card={card} />
         ))}
       </View>
     </View>
@@ -93,9 +88,6 @@ export default CardPack;
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    // width: '100%',
-    // height: '100%'
   },
-  cardContainer: {
-  },
+  cardContainer: {},
 });
